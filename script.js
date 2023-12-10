d91665670fa12de876ae846538fc8d1c303c32a2
Pellepurck <64706029+Pellepurck@users.noreply.github.com>
2023-12-10 15:46:39 +0100

    Sticky navbar

--- script.js
+++ script.js
@@ -1,26 +1,14 @@
-let backVideo = document.getElementById("backVideo");
-    let btnicon = document.getElementById("btnicon");
+// script.js
 
-    btnicon.onclick = function(){
-        backVideo.style.display = "block";
+document.addEventListener('DOMContentLoaded', function () {
+  var header = document.querySelector('.header');
+  var headerHeight = header.clientHeight;
 
-        if(backVideo.paused){
-            backVideo.play();
-            btnicon.src = "icons/pause.png";
-        }
-        else{
-            backVideo.pause();
-            btnicon.src = "icons/play.png";
-        }
-    }
-/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("header").style.top = "0";
-  } else {
-    document.getElementById("header").style.top = "-50px";
-  }
-  prevScrollpos = currentScrollPos;
-}
+  window.addEventListener('scroll', function() {
+      if (window.scrollY > headerHeight) {
+          header.classList.add('sticky');
+      } else {
+          header.classList.remove('sticky');
+      }
+  });
+});
